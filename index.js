a242836dcf1d2d76395ccd23246592f9a0872fbb
sowmik-sec <sowmik.sec@gmail.com>
2023-03-09 10:40:09 +0600

    data stored in mongodb server

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// user: dbuser2
+// pass: jTqDJbMl6b3MeAct
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbuser2:jTqDJbMl6b3MeAct@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const userCollection = client.db("nodeMongoCrud").collection("users");
+    const user = {
+      name: "testing test",
+      email: "testing@test.com",
+    };
+    const result = await userCollection.insertOne(user);
+    console.log(result);
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from node mongo crud server");
+});
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
